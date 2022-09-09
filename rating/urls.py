5c2e618169ae7075545143844776f9a776edd7d1
Durbek11 <pattoyevd@gmail.com>
2022-09-09 15:08:11 +0500

    Durbek

---
+++ rating/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'blog'
+urlpatterns = [
+    path('', home, name='home')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ static/js/js/main.js
@@ -0,0 +1,7 @@
+const one = document.getElementById("first")
+const two = document.getElementById("second")
+const three = document.getElementById("thrid")
+const four = document.getElementById("fourth")
+const five = document.getElementById("fifth")
+
+const staraArr = [one, two, three, four, five]
