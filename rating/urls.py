f34ab756a1a07c803a9b3f458289249993cb86cb
Durbek11 <pattoyevd@gmail.com>
2022-09-12 14:18:41 +0500

    commit

--- rating/urls.py
+++ rating/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 
 app_name = 'blog'
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('rate-img/', rate_img, name="rate")
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rating/views.py
+++ rating/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 def home(request):
     rate = RateStar.objects.filter(score=2).order_by("?").first()
@@ -7,3 +8,12 @@ def home(request):
         "rate": rate
     }
     return render(request, "home.html", context )
+
+def rate_img(request):
+    if request.method == "POST":
+        el_vd = request.POST.get("el_vd")
+        rate = RateStars.objects.get(id=el_vd)
+        rate.score = val
+        rate.save()
+        return JsonResponse({"success": "true", "score": val}, safe=False)
+    return JsonResponse({"success": "false"})
