0ebb7f35872c2f0d2c73c924c5fc8a39cee54b84
Durbek11 <pattoyevd@gmail.com>
2022-09-19 14:44:47 +0500

    inital commit

--- rating/views.py
+++ rating/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Avg
 
+# def home(request):
+#     rate = RateStar.objects.filter(score=2).order_by("?").first()
+#     context = {
+#         "rate": rate
+#     }
+#     return render(request, "home.html", context )
+
+# def rate_img(request):
+#     if request.method == "POST":
+#         el_id = request.POST.get("el_id")
+#         val = request.POST.get("val")
+#         rate = RateStar.objects.get(id=el_id)
+#         rate.score = val
+#         rate.save()
+#         return JsonResponse({"success": "true", "score": val}, safe=False)
+#     return JsonResponse({"success": "false"})
 def home(request):
     rate = RateStar.objects.filter(score=2).order_by("?").first()
+    avg_rate = RateStar.objects.aggregate(Avg("score"))
     context = {
-        "rate": rate
+        "rate": rate,
+        "avg_rate": avg_rate
     }
     return render(request, "home.html", context )
 
